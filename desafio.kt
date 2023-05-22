0a7d0604f16f749f2a5e745d2f4fef7b1293b7fe
Thiago Aureliano <83797121+thiagoaure@users.noreply.github.com>
2023-05-22 17:45:39 -0300

    fix(all): adiciona mudanças para testes da aplicação

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,36 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String, val numeroDocumento: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60, val nivel: Nivel)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+	fun matricular(vararg usuario: Usuario) {
+        inscritos.addAll(usuario)
+	}
+    
+    fun printAlunosMatriculados() {
+        println("Alunos matriculados no curso de $nome: ")
+		inscritos.forEach { aluno ->
+        	println(aluno.nome)
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    val java = ConteudoEducacional("Java", 70, Nivel.INTERMEDIARIO)
+    val kotlin1 = ConteudoEducacional("Kotlin 1", 80, Nivel.BASICO)
+    val kotlin2 = ConteudoEducacional("Kotlin 2", 100, Nivel.INTERMEDIARIO)
+    val kotlin3 = ConteudoEducacional("Kotlin 3", 100, Nivel.DIFICIL)
+    val conteudo = listOf<ConteudoEducacional>()
+    conteudo.plus(listOf(java, kotlin1, kotlin2, kotlin3 ))
+    val formacaoKotlin = Formacao("Formacao em Kotlin", conteudo, Nivel.INTERMEDIARIO)
+    val Aluno = Usuario("Thiago", "1111122299")
+    val Aluno2 = Usuario("Renato", "33333322299")
+    formacaoKotlin.matricular(Aluno, Aluno2)
+    formacaoKotlin.printAlunosMatriculados()
 }
